36dae79714c93a3bf2036b2bba617d0b4fd6b126
tapriandi <tapriandi@gmail.com>
2024-02-03 09:58:04 +0700

    day-2

---
+++ day-2.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < 10; i++) {
+  console.log("hello world " + i);
+}
+
+
+console.log("hello world ");
+console.log("hello world ");
+console.log("hello world ");
+console.log("hello world ");
+console.log("hello world ");
+console.log("hello world ");
+console.log("hello world ");
+console.log("hello world ");
